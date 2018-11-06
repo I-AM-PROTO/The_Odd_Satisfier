35775e2d0e00fa368ab8fc5148147bb6aa2fa198
Nyro Kim <38270326+I-AM-PROTO@users.noreply.github.com>
2018-11-06 13:11:28 +0900

    Update gameboard.java

--- gameboard.java
+++ gameboard.java
@@ -1,7 +1,20 @@
 package snake_gameboard;
 
-public class gameboard {
-	public static void main (String[] args) {
-		
+public class Snake_gameboard {
+	private int[][] gameboard;
+	private int size;
+	
+	public Snake_gameboard(int size) {
+		this.size = size;
+		gameboard = new int[size][size];
+	}
+	
+	public void displayBoard() {
+		for(int i=0; i<size; i++){
+			for(int j=0; j<size; j++) {
+				System.out.print("□");
+			}
+			System.out.println();
+		}
 	}
 }
