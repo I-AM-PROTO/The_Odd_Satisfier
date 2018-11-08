80fb51e37d1c746c8670b956c6567fba96f5c7bb
Nyro Kim <38270326+I-AM-PROTO@users.noreply.github.com>
2018-11-08 17:21:56 +0900

    Update Snake_game.java

--- Snake_game.java
+++ Snake_game.java
@@ -2,6 +2,30 @@ package snake_gameboard;
 
 public class Snake_game {
 	
+	//settings method - board size, speed, etc
+	
+	//getting input method
+	
+	//move method + check if lost
+	
+	//check if win
+	
+	//check if lose
+	
+	void run() {
+		//start in the middle
+		
+		//infinite loop
+		/*
+		 * if no input -> keep direction (default right)
+		 * else if input -> change direction to input 
+		 * 
+		 * move & check if lost
+		 * check if victory == check if full snake
+		 */
+		//end loop
+	}
+	
 	public static void main (String[] args) {
 		Snake_gameboard game = new Snake_gameboard(3);
 		game.displayBoard();
