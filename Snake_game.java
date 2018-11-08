cb8c3ba48d7d1c1e008e96dd1479a7c9dfcdec90
Nyro Kim <38270326+I-AM-PROTO@users.noreply.github.com>
2018-11-08 23:57:53 +0900

    Update Snake_game.java

--- Snake_game.java
+++ Snake_game.java
@@ -1,19 +1,47 @@
 package snake_gameboard;
 
+enum Direction {
+	LEFT(0,-1), RIGHT(0, 1), UP(-1, 0), DOWN(1, 0);
+	
+	private int xmove, ymove;
+	private Direction(int xmove, int ymove) { this.xmove = xmove; this.ymove = ymove; }
+}
+
 public class Snake_game {
+	final private double defaultInterval = 500;
+	private double speed;
+	private double inGameInterval; //interval between snake moves
+	private int gameWidth, gameHeight; // where snake can move (excludes walls)
+	private int growthLen; // turn left for snake to grow (3 each apple, i guess)
+	private Snake_gameboard gameBoard;
+	private Block head, tail;
 	
 	//settings method - board size, speed, etc
 	
 	//getting input method
 	
-	//move method + check if lost
+	//true:ded false:alive
+	boolean moveSnake () {
+		
+		
+		return false;
+	}
 	
-	//check if win
+	//true:Victory, game ends false:still ingame
+	boolean checkIfWin() {
+		return false;
+	}
 	
-	//check if lose
+	//get settings, create board, put head in center, direction right
+	public Snake_game(double speed, int gameWidth, int gameHeight) {
+		this.speed = speed;
+		this.inGameInterval = defaultInterval / speed;
+		this.gameWidth = gameWidth;
+		this.gameHeight = gameHeight;
+		gameBoard = new Snake_gameboard(gameWidth, gameHeight);
+	}
 	
 	void run() {
-		//start in the middle
 		
 		//infinite loop
 		/*
@@ -27,7 +55,7 @@ public class Snake_game {
 	}
 	
 	public static void main (String[] args) {
-		Snake_gameboard game = new Snake_gameboard(3);
+		Snake_gameboard game = new Snake_gameboard(15, 10);
 		game.displayBoard();
 	}
 }
