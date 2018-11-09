7493e617a06ce5fadacdd0ad11774a3612f58c86
Nyro Kim <38270326+I-AM-PROTO@users.noreply.github.com>
2018-11-09 23:07:57 +0900

    Update Snake_gameboard.java

--- Snake_gameboard.java
+++ Snake_gameboard.java
@@ -16,8 +16,8 @@ class Block {
 	public Block (int xpos, int ypos) { this.type = BlockType.BLANK; this.xpos = xpos; this.ypos=ypos; }
 	
 	//set and get field
-	public void setBlockType (BlockType type) { this.type = type; }
-	public void setDirection (Direction dir) { this.dir = dir; }
+	public void setBlockData (BlockType type) { this.type = type; }
+	public void setBlockData (Direction dir) { this.dir = dir; }
 	public BlockType getBlockType () { return type; }
 	public Direction getDirection () { return dir; }
 	public int getxpos () { return xpos; }
@@ -29,20 +29,23 @@ public class Snake_gameboard {
 	private int width, height;
 	
 	//constructor
-	public Snake_gameboard(int width, int height) {
-		this.width = width += 2; // added 2 considering wall on all four sides
+	public Snake_gameboard(int height, int width) {
+		this.width = width += 2; // add 2 considering wall on all four sides
 		this.height = height += 2;
 		gameBoard = new Block[this.height][this.width];
 		initializeGameBoard();
 	}
 	
-	//first game board setting - blanks, walls and head in the center
+	//first game board setting
 	private void initializeGameBoard() {
+		
+		//set blanks
 		for(int i=1; i<height; i++) {
 			for(int j=1; j<width; j++)
 				gameBoard[i][j] = new Block(i, j);
 		}
 		
+		//set walls
 		for(int i=0; i<height; i++) {
 			gameBoard[i][0] = new Block(BlockType.WALL, i, 0);
 			gameBoard[i][width-1] = new Block(BlockType.WALL, i, width-1);
@@ -53,11 +56,15 @@ public class Snake_gameboard {
 			gameBoard[height-1][i] = new Block(BlockType.WALL, height-1, i);
 		}
 		
-		gameBoard[(height-1)/2][(width-1)/2].setBlockType(BlockType.HEAD);
-		gameBoard[(height-1)/2][(width-1)/2].setDirection(Direction.RIGHT);
+		//put head on center - goes right by default
+		gameBoard[(height-1)/2][(width-1)/2].setBlockData(BlockType.HEAD);
+		gameBoard[(height-1)/2][(width-1)/2].setBlockData(Direction.RIGHT);
+		//put tail(body) on the left of head
+		gameBoard[(height-1)/2][(width-1)/2 - 1].setBlockData(BlockType.BODY);
+		gameBoard[(height-1)/2][(width-1)/2 - 1].setBlockData(Direction.RIGHT);
 	}
 	
-	//displays board - have to change to GUI later
+	//display board - have to change to GUI later
 	public void displayBoard() {
 		for(int i=0; i<height; i++){
 			for(int j=0; j<width; j++) {
@@ -67,18 +74,23 @@ public class Snake_gameboard {
 		}
 	}
 
-	//gives block type
+	//gives block
 	public Block getBlock(int x, int y) { return gameBoard[x][y]; }
 	
-	//gets and changes block type
-	public void setBlock(int x, int y, Block type) { gameBoard[x][y] = type; }
+	//gets and changes block data
+	public void setBlock(int x, int y, BlockType type) { gameBoard[x][y].setBlockData(type); }
+	public void setBlock(int x, int y, Direction dir) { gameBoard[x][y].setBlockData(dir); }
 	
 	//whether snake can exist or not
-	public boolean isValid(int x, int y) {
-		if(0 < x && x < width-1 && 0 < y && y < height-1 && gameBoard[x][y].getBlockType() != BlockType.BODY)
+	public boolean isValidHead(int x, int y) {
+		if(0 < x && x < height-1 && 0 < y && y < width-1 && gameBoard[x][y].getBlockType() != BlockType.BODY)
 			return true;
 		else
 			return false;
+	}
+	
+	public boolean isValidBlank(int x, int y) {
+		return 0 < x && x < height-1 && 0 < y && y < width-1 && gameBoard[x][y].getBlockType() == BlockType.BLANK;
 	}
 
 	//have to change into GUI later
