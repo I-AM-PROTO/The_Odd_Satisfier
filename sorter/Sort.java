fcc6f88dc5bc39f484d62d12bdd35c4e430c4a9e
unknown <kty2396@gmail.com>
2018-12-07 11:51:52 +0900

    insertion & merge update

--- sorter/Sort.java
+++ sorter/Sort.java
@@ -2,7 +2,7 @@ package sorter;
 
 import java.util.Vector;
 
-public class Sort {
+public abstract class Sort {
 	protected Vector<Integer> v;
 	protected int size;
 	protected int[] candidate = {-1, -1};
@@ -75,5 +75,75 @@ class SelectionSort extends Sort {
 		
 		if(i == size-1)
 			isSorted = true;
+	}
+}
+
+class InsertionSort extends Sort {
+	private int i = 1, j = 1;
+	public InsertionSort(Vector<Integer> v, int size) { super(v, size); }
+	
+	public void takeAStep() {
+		for(; j>0; j--) {
+			if(v.get(j) < v.get(j-1)) {
+				candidate[0] = j-1;
+				candidate[1] = j;
+				swap(j-1, j);
+				return;
+			}
+		}
+		
+		i++; j = i;
+		if(i == size) {
+			isSorted = true;
+			return;
+		}
+		else
+			takeAStep();
+	}
+}
+
+class MergeSort extends Sort {
+	private int i = 0, j;
+	private int maxLayer = 0, layerIndex = 1, boxIndex = 1, limit = 1;
+	public MergeSort(Vector<Integer> v, int size) {
+		super(v, size);
+		while(Math.pow(2, maxLayer) < size)
+			maxLayer++;
+	}
+	
+	public void takeAStep() {
+		System.out.printf("Layer:%d Box:%d i:%d limit:%d\n", layerIndex, boxIndex, i, limit);
+		int min;
+		
+		do {
+			i++;
+			if(i == limit) {
+				i++;
+				boxIndex++;
+				limit = (int)Math.pow(2, layerIndex) * boxIndex - 1;
+
+				if(limit >= size) {
+					i = 0;
+					boxIndex = 1;
+					layerIndex++;
+					limit = Math.min(size - 1, (int)Math.pow(2, layerIndex) * boxIndex - 1);
+
+					if(layerIndex > maxLayer) {
+						isSorted = true;
+						return;
+					}
+				}
+			}
+			min = i;
+			for(j = i + 1; j <= limit; j++) {
+				if(v.get(min) > v.get(j))
+					min = j;
+			}
+		}while(min == i);
+
+		candidate[0] = i;
+		candidate[1] = min;
+		swap(min, i);
+
 	}
 }
